f72d37a01c1b45e502be77ff25d1d7afba786bc0
Ilya Volkov <102101100@mail.ru>
2020-09-27 11:53:43 +0300

    create: Project13 ready for review 2

--- controllers/users.js
+++ controllers/users.js
@@ -4,15 +4,19 @@ const getAllUsers = (req, res) => {
   User.find({})
     .then((user) => res.send({ data: user }))
     .catch(() => {
-      res.status(500).send({ message: 'Что-то пошло не так...' });
+      res.status(500).send({ message: 'Ошибка сервера. Что-то пошло не так...' });
     });
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params.id)
     .then((user) => res.send({ data: user }))
+    // eslint-disable-next-line consistent-return
     .catch((err) => {
-      if (err.name === 'CastError') return res.status(404).send({ message: 'пользователь не найден!' });
+      if (err.name === 'CastError') {
+        return res.status(404).send({ message: 'пользователь не найден!' });
+      }
+      return res.status(500).send({ message: 'Ошибка сервера. Что то пошло не так..' });
     });
 };
 
@@ -20,8 +24,12 @@ const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
+    // eslint-disable-next-line consistent-return
     .catch((err) => {
-      if (err.name === 'ValidationError') return res.status(400).send({ message: 'переданы некорректные данные в методы создания пользователя!' });
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'переданы некорректные данные в методы создания пользователя!' });
+      }
+      return res.status(500).send({ message: 'Ошибка сервера. Что то пошло не так..' });
     });
 };
 
@@ -30,5 +38,3 @@ module.exports = {
   getUserById,
   createUser,
 };
-
-//5f6f660b3ca10ae2b08d9e96
